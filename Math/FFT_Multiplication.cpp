376a2dafda43907d8a13d5eb3ba8d0adda1947d9
toxicpie <kent05011607@gmail.com>
2019-10-04 03:13:38 +0800

    added debug branch

--- Math/FFT_Multiplication.cpp
+++ Math/FFT_Multiplication.cpp
@@ -1,37 +1,89 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
 typedef long double ld;
 typedef complex<ld> c;
+// polynomial
 typedef vector<c> poly;
-const ld pi = 3.141592653589793238;
 
-int n;
+const ld pi = 3.1415926535897932384626433832795;
 
-void fft(poly &f, int a, int d, bool inv) {
-    if(d==n) return;
-    fft(f, a, d*2, inv);
-    fft(f, a+d, d*2, inv);
-    poly res(f.size());
-    ld r = 2*pi*(inv?d:-d)/n;
+// fft of polynomial f, processing n entries starting from a with common difference d
+// inv: whether it's inverse fft
+void fft(poly &f, int n, int a, int d, bool inv) {
+    if(n==1) return;
+
+    // even and odd
+    fft(f, n/2, a, d*2, inv);
+    fft(f, n/2, a+d, d*2, inv);
+
+    // combine
+    poly res(f.size(), 0);
+    // w = n-th root of unity
+    ld arg = 2*pi*(inv?1:-1) / n;
+    c wn = c(cos(arg), sin(arg));
     c w = c(1, 0);
-    for(int i=a; i<n/2; i+=d) {
-        c even = f[2*i-a];
-        c odd = w * f[2*i+d-a];
-        res[i] = even + odd;
-        res[i+n/2] = even - odd;
-        w *= c(cos(r), sin(r));
+    for(int i=0; i<n/2; i++) {
+        c even = f[a+(2*i)*d];
+        c odd = w * f[a+(2*i+1)*d];
+        res[a+i*d] = even + odd;
+        res[a+(i+n/2)*d] = even - odd;
+        // in inverse fft, we do the a=even+odd, b=even-odd step in reverse
+        // which yields even=(a+b)/2 and odd=(a-b)/2
+        if(inv) {
+            res[a+i*d] /= 2;
+            res[a+(i+n/2)*d] /= 2;
+        }
+        w *= wn;
     }
-    for(int i=a; i<n; i+=d)
-        f[i] = res[i] / (c)(inv?2:1);
+    for(int i=0; i<n; i++){
+        f[a+d*i] = res[a+d*i];
+    }
 }
 
 poly mult(poly &f, poly &g) {
-    n = 1<<(__lg(f.size()+g.size())+1);
-    f.resize(n);
-    g.resize(n);
-    fft(f, 0, 1, 0);
-    fft(g, 0, 1, 0);
+    // length must be 2^n
+    int len = 1 << (__lg(f.size() + g.size()) + 1);
+    f.resize(len);
+    g.resize(len);
+    fft(f, len, 0, 1, false);
+    fft(g, len, 0, 1, false);
     poly res;
-    for(int i=0; i<n; i++)
+    for(int i=0; i<len; i++) {
         res.push_back(f[i]*g[i]);
-    fft(res, 0, 1, 1);
+    }
+    fft(res, len, 0, 1, true);
     return res;
+}
+
+// input: two lines
+//     first line: an integer m, then m numbers f_i
+//     denoting f = \sum f_i x^i
+//     second line: an integer n, then n numbers g_i
+//     denoting g = \sum g_i x^i
+// output: one line containing m+n-1 numbers p_i, representing the product p = \sum p_i x^i
+// time complexity: O(nlogn), where n is the greater of f and g's degrees
+int main() {
+    int m, n;
+    ld x;
+    poly f, g;
+    cin >> m;
+    for(int i=0; i<m; i++) {
+        cin >> x;
+        f.push_back(x);
+    }
+    cin >> n;
+    for(int i=0; i<n; i++) {
+        cin >> x;
+        g.push_back(x);
+    }
+    poly res = mult(f, g);
+    cout << fixed << setprecision(4);
+    for(int i=0; i<m+n-1; i++) {
+        if(i) cout << ' ';
+        cout << res[i].real();
+    }
+    cout << '\n';
+    return 0;
 }
--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # NCTU_Hikari's Codebook
 
+**NOTE**: This is the debug branch, in which the code are generally more readable and has input that allows debugging. See the master branch for versions that are more suitable for a codebook.
+
 A collection of code that are useful in programming contests.
 
 Since we're a new team, many algorithms/concepts are still being learned and will be added to the collection progressively.
